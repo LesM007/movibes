ffaa3c43180bda8f50f29d16a25c4e7e886c0142
LesM007 <u1501857@rts-365.dk>
2023-01-21 13:49:56 +0100

    react-icons

--- src/templates/Navigation.js
+++ src/templates/Navigation.js
@@ -1,2 +1,20 @@
 import { NavLink } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
 
+
+
+const Navigation = () => {
+
+    const activeStyle = {
+        textDecoration: "underline",
+    };
+
+    return ( 
+        <>
+    <NavLink to="/" ><FaHome />Home</NavLink>
+    <NavLink to="/tvseries"  >TV Series</NavLink>
+    <NavLink to="/upcoming"  >Upcoming</NavLink>
+    </>
+       );
+}
+export default Navigation;
